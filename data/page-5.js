281c1c84125172e9c5dc62268a02b547f95aa531
Michael Ignacio <mikelignacio@gmail.com>
2022-11-04 00:07:42 +0800

    Add page 5

---
+++ data/page-5.js
@@ -0,0 +1,38 @@
+const days = [
+  {
+    date: 'aug 3, wed',
+    items: [
+      {
+        indicator: '•',
+        name: 'sort out car docs',
+      },
+      {
+        indicator: '-',
+        name: 'talasalitaan w/ Marian: pisi, kubli, antipara',
+      },
+    ],
+  },
+  {
+    date: 'aug 4, thu',
+    items: [
+      {
+        indicator: '*',
+        name: 'wish Marian a safe flight!',
+      },
+      {
+        indicator: '•',
+        name: 'dentist appt',
+      },
+      {
+        indicator: '•',
+        name: 'hit the gym',
+      },
+      {
+        indicator: '>',
+        name: 'get diploma',
+      },
+    ],
+  },
+];
+
+export default days;
---
+++ pages/page-5.js
@@ -0,0 +1,19 @@
+import log from '../data/page-5'
+import Page from '../components/page'
+import featured from '../public/i-like-you.png'
+
+const spotify = (
+  <iframe
+    src="https://open.spotify.com/embed/track/0O6u0VJ46W86TxN9wgyqDj?utm_source=generator&theme=0"
+    width="100%"
+    height="152"
+    frameBorder="0"
+    allowfullscreen=""
+    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+    loading="lazy"
+  ></iframe>
+);
+
+export default function Day4() {
+  return <Page log={log} spotify={spotify} featured={featured} pageNum="5" />;
+}
