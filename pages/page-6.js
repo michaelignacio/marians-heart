8619103b0ce39da66fb981d4db776bbe9252959f
Michael Ignacio <mikelignacio@gmail.com>
2022-11-04 08:40:06 +0800

    Update song

--- pages/page-6.js
+++ pages/page-6.js
@@ -1,10 +1,10 @@
 import log from '../data/page-6'
 import Page from '../components/page'
-import featured from '../public/capy.jpg'
+import featured from '../public/umaapaw.png'
 
 const spotify = (
   <iframe
-    src="https://open.spotify.com/embed/track/5iMRhOHzze2CujxHiJ6cRL?utm_source=generator&theme=0"
+    src="https://open.spotify.com/embed/track/4UOfFouPDw0AqVWijbW2ea?utm_source=generator&theme=0"
     width="100%"
     height="152"
     frameBorder="0"
