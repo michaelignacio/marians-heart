a35fe3687f93fa9dc266c3ef624dc7c7138ec6d8
Michael Ignacio <mikelignacio@gmail.com>
2022-11-04 10:18:52 +0800

    Update order

--- data/page-6.js
+++ data/page-6.js
@@ -36,12 +36,12 @@ const days = [
         name: 'weekend date with Marian!',
       },
       {
-        indicator: '*',
-        name: 'DTR',
-      },
-      {
         indicator: '-',
         name: "check out Conti's food",
+      },
+      {
+        indicator: '*',
+        name: 'DTR',
       },
     ],
   },
