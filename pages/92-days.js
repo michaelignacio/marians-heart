8e824578dfa134640aef17fbbb932d92685fa421
Michael Ignacio <mikelignacio@gmail.com>
2022-11-07 15:29:45 +0800

    Update page

--- pages/92-days.js
+++ pages/92-days.js
@@ -10,15 +10,14 @@ export default function DaysOfMarian() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className="flex justify-center items-center p-5">
-        <div className="max-w-2xl w-full bg-amber-100 p-10 rounded-l-xl">
+        <div className="max-w-2xl w-full bg-amber-100 p-7 xl:p-10 rounded-l-xl">
           <div className="flex flex-col">
             <div className="handwriting flex-col-reverse flex lg:flex-col">
               <div className="index-content"></div>
               <div className="title">
-                <div className="leading-10 flex gap-5 w-full mt-10">
-                  <div className="border-b w-full text-center text-5xl text-stone-600">
-                    <p className="w-4/5 mx-auto">
-                      <p className="text-3xl">
+                <div className="leading-10 flex gap-5 w-full">
+                  <div className="w-full text-center text-stone-600">
+                      <p className="text-2xl md:text-3xl">
                         You know how I&apos;m so captivated by you that I want
                         to profess my love for you across all mediums and platforms?
                         Here&apos;s another one:
@@ -27,11 +26,10 @@ export default function DaysOfMarian() {
                         src={marian}
                         className={`my-5 rounded-xl w-1/2 mx-auto`}
                       />
-                      <p className="text-3xl">
+                      <p className="text-4xl">
                         I love you, my beautiful Marian (in chibi world). Happy
                         third monthsary 😘
                       </p>
-                    </p>
                   </div>
                 </div>
               </div>
